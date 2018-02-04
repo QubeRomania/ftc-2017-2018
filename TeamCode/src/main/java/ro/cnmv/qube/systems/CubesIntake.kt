e90517869b7dc84efa6f866772049ed6b14cea62
Gabriel Majeri <gabriel.majeri6@gmail.com>
2018-02-04 13:05:34 +0200

    Add CubeIntake control code

---
+++ TeamCode/src/main/java/ro/cnmv/qube/systems/CubesIntake.kt
@@ -0,0 +1,30 @@
+package ro.cnmv.qube.systems
+
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.Gamepad
+
+interface CubesIntake {
+    val intakeLeft: DcMotor
+    val intakeRight: DcMotor
+    val intakeOpen: DcMotor
+
+    /// Opens and closes the intake funnel.
+    fun open(gp: Gamepad) {
+        intakeOpen.power = when {
+            gp.x -> 1.0
+            gp.y -> -1.0
+            else -> 0.0
+        }
+    }
+
+    /// Controls cube intake.
+    fun intake(gp: Gamepad) {
+        intakeLeft.power = roundPower(gp.left_stick_y)
+        intakeRight.power = roundPower(gp.right_stick_y)
+    }
+
+    companion object {
+        /// Rounds a number to nearest multiple of 0.5
+        fun roundPower(power: Float): Double = Math.round(power * 2) / 2.0
+    }
+}
