d0ad61a2be1e629ac40ce8d6a23c38838f6fe432
Gabriel Majeri <gabriel.majeri6@gmail.com>
2018-02-04 16:22:16 +0200

    Add autonomies

---
+++ TeamCode/src/main/java/ro/cnmv/qube/autonomy/AutonomyBase.kt
@@ -0,0 +1,9 @@
+package ro.cnmv.qube.autonomy
+
+import ro.cnmv.qube.core.RobotOpMode
+
+open class AutonomyBase: RobotOpMode() {
+    override fun runOpMode() {
+        waitForStart()
+    }
+}
---
+++ TeamCode/src/main/java/ro/cnmv/qube/autonomy/AutonomyFarRed.kt
@@ -0,0 +1,7 @@
+package ro.cnmv.qube.autonomy
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+
+@Autonomous(name = "Autonomy Far Blue", group = "Autonomies")
+class AutonomyFarRed: AutonomyBase() {
+}
