2e5d851a96e9e19591f2e4fd41ba80ec8a667fe6
QUBE <andrei.daha@cnmv.ro>
2018-02-06 11:28:58 +0200

    Fix Vuforia crash

--- TeamCode/src/main/java/ro/cnmv/qube/systems/Vuforia.kt
+++ TeamCode/src/main/java/ro/cnmv/qube/systems/Vuforia.kt
@@ -4,10 +4,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark
 
 interface Vuforia {
     /// Activates the localizer.
-    fun activate();
+    fun activate()
 
     /// Deactivates the localizer.
-    fun deactivate();
+    fun deactivate()
 
     /// The currently detected VuMark.
     val vuMark: RelicRecoveryVuMark
