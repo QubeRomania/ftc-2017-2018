ee36f9dca47beaafb5780a6e23f540df261c48c0
QUBE <andrei.daha@cnmv.ro>
2018-02-06 11:28:07 +0200

    Fix autonomies with same name

--- TeamCode/src/main/java/ro/cnmv/qube/autonomy/AutonomyFarRed.kt
+++ TeamCode/src/main/java/ro/cnmv/qube/autonomy/AutonomyFarRed.kt
@@ -2,7 +2,7 @@ package ro.cnmv.qube.autonomy
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 
-@Autonomous(name = "Autonomy Far Blue", group = "Autonomies")
+@Autonomous(name = "Autonomy Far Red", group = "Autonomies")
 class AutonomyFarRed: AutonomyBase() {
     override val directionSign = -1.0
 }
