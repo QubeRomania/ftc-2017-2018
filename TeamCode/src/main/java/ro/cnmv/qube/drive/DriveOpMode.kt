ec273c53081887f730500ba1e64e1b765753e3db
QUBE <andrei.daha@cnmv.ro>
2018-02-07 13:19:37 +0200

    Fix drive mode

--- TeamCode/src/main/java/ro/cnmv/qube/drive/DriveOpMode.kt
+++ TeamCode/src/main/java/ro/cnmv/qube/drive/DriveOpMode.kt
@@ -6,16 +6,16 @@ import ro.cnmv.qube.core.RobotOpMode
 @TeleOp(name = "Complete Drive", group = "Drive")
 class DriveOpMode: RobotOpMode() {
     override fun runOpMode() {
-        robot.setEncoders(false)
-
         waitForStart()
-
         while (opModeIsActive()) {
             // DRIVE
             robot.driveWithGamepad(gamepad1)
 
             // CUBES INTAKE
             robot.intakeWithGamepad(gamepad2)
+
+            // CUBES LIFT
+            robot.liftWithGamepad(gamepad2)
 
             // CUBES DROP
             robot.dropWithGamepad(gamepad2)
