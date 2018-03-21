8e77d548faf7f9bf23b3af0f7b752f7eab08bce7
Matei Chesa <chesamatei@yahoo.ro>
2018-03-21 17:21:26 +0200

    Update cube lifting code

--- TeamCode/src/main/java/ro/cnmv/qube/systems/CubesLift.kt
+++ TeamCode/src/main/java/ro/cnmv/qube/systems/CubesLift.kt
@@ -1,25 +1,50 @@
 package ro.cnmv.qube.systems
 
-import com.qualcomm.robotcore.hardware.CRServo
 import com.qualcomm.robotcore.hardware.DcMotor
-import com.qualcomm.robotcore.hardware.Gamepad
+import ro.cnmv.qube.core.Gamepad
+import ro.cnmv.qube.core.GamepadButton
+import ro.cnmv.qube.core.OpModeAccess
 
-interface CubesLift {
+interface CubesLift: OpModeAccess {
     val liftMotor: DcMotor
 
-    var power: Double
+    companion object {
+        const val LIFT_SPEED = 1
 
-    fun liftWithGamepad(gp: Gamepad) {
-        power = when {
-            gp.a -> -0.5
-            gp.b -> 0.5
-            else -> 0.0
-        }
-
-        lift(power)
+        const val LIFT_BOTTOM = 0
+        // TODO: MIDDLE
+        const val LIFT_TOP = 1500
     }
 
-    fun lift(power: Double) {
-        liftMotor.power = power
+    fun liftWithGamepad(gp: Gamepad) {
+        if (gp.checkButtonToggle(GamepadButton.LEFT_BUMPER)) {
+            tele.addData("Mode", "manual")
+            // Manual mode
+            var newPosition = liftMotor.currentPosition + when {
+                gp.checkButtonHold(GamepadButton.A) -> +LIFT_SPEED
+                gp.checkButtonHold(GamepadButton.B) -> -LIFT_SPEED
+                else -> 0
+            }
+
+            if (newPosition < LIFT_BOTTOM) {
+                newPosition = LIFT_BOTTOM
+            } else if (newPosition > LIFT_TOP) {
+                newPosition = LIFT_TOP
+            }
+
+            liftPosition(newPosition)
+        } else {
+            tele.addData("Mode", "auto")
+            // Auto mode
+            when {
+                gp.checkButtonHold(GamepadButton.A) -> liftPosition(LIFT_TOP)
+                gp.checkButtonHold(GamepadButton.B) -> liftPosition(LIFT_BOTTOM)
+            }
+        }
+    }
+
+    fun liftPosition(position: Int) {
+        liftMotor.targetPosition = position
+        liftMotor.mode = DcMotor.RunMode.RUN_TO_POSITION
     }
 }
